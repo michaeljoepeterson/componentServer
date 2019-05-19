493d32f272de8c7ca043718e9f3b7a4fe0af5862
Michael Peterson <michaeljoepeterson@gmail.com>
2019-05-19 16:08:28 -0600

    initial setup for checking styles

--- public/slideShow/engine/engine.js
+++ public/slideShow/engine/engine.js
@@ -3,6 +3,8 @@ function Engine(options){
 	this.options = options;
 	this.slideShowStyles = document.styleSheets[0].cssRules;
 	console.log(this.slideShowStyles);
+	this.initCSS();
+	//this.slideShowStyles[1].style.width = "50%";
 }
 
 Engine.prototype.initEngine = function() {
@@ -10,7 +12,15 @@ Engine.prototype.initEngine = function() {
 };
 
 Engine.prototype.initCSS = function() {
-	
+	for(let i = 0;i < this.slideShowStyles.length;i++){
+		if(this.slideShowStyles[i].type === 1){
+			let styleName = this.slideShowStyles[i].selectorText.replace(".","");
+			console.log(styleName,this.slideShowStyles[i].type);
+			if(this.options.cssData[styleName]){
+				console.log("found style",this.options.cssData[styleName]);
+			}
+		}	
+	}
 };
 
 let engine = new Engine(optionData);
