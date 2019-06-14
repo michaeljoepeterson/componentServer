88a9a99429a57ec1f15ab2fc29704dac9260fd08
Michael Peterson <michaeljoepeterson@gmail.com>
2019-06-13 19:35:09 -0600

    added initial data file and more styles to nav

---
+++ public/nav/js/navData.js
@@ -0,0 +1,23 @@
+//use this object to hold options for css, javascript, and html?
+const navData = {
+	//all the various classes and their options
+	cssData:{
+		"nav-container":{
+			backgroundColor: "rgb(245,245,245)",
+			height: "55px",
+		},
+		"nav-items":{
+			top: "13.5px"
+		}
+	},
+	mediaQueries:{
+
+	},
+
+	htmlData:{
+
+	},
+	jsData:{
+
+	}
+};
